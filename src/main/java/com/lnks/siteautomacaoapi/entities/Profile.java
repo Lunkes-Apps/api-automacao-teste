cf9bb8f4a72a0f63ee162abf4d912b81a44fbe2c
Alexandre Lunkes <lunkes.alexandre@gmail.com>
2020-05-24 19:19:46 -0300

    adicionando entities e enuns

---
+++ src/main/java/com/lnks/siteautomacaoapi/entities/Profile.java
@@ -0,0 +1,30 @@
+package com.lnks.siteautomacaoapi.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Table(name="perfil")
+@Data
+public class Profile implements Serializable{
+
+	private static final long serialVersionUID = 3827663714334497998L;
+	
+	 private @Id @GeneratedValue(strategy=GenerationType.AUTO) Long id;
+	 private @Column(name="nome", nullable=false) String nome;
+     private @Column(name="email", nullable=false) String email;
+     private @Column(name="email", nullable=false) String password;
+     	
+
+}
---
+++ src/main/java/com/lnks/siteautomacaoapi/enuns/PerfilEnum.java
@@ -0,0 +1,8 @@
+package com.lnks.siteautomacaoapi.enuns;
+
+
+public enum PerfilEnum {
+	ROLE_ADMIN,
+	ROLE_USUARIO;
+
+}
