5688b3edb9c373714038e8be9ea8b3cc3e059800
Alexandre Lunkes <lunkes.alexandre@gmail.com>
2020-05-25 15:13:06 -0300

    Adicionando flyway

---
+++ src/main/resources/db/migration/mysql/V1__init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `perfil` (
+	`id` bigint(20) NOT NULL,
+	`nome` varchar(255) NOT NULL,
+	`email` varchar(255) NOT NULL,
+	`password` varchar(255) NOT NULL
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `perfil` ADD PRIMARY KEY(`id`);
+ALTER TABLE `perfil` MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
