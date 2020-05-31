31b8c5dcad959c39651bef67822684b5dbe8571a
Alexandre <lunkes.alexandre@gmail.com>
2020-05-31 02:17:45 -0300

    Correção do bug - lista null

--- src/main/java/com/lnks/siteautomacaoapi/controllers/CadastroUsuarioController.java
+++ src/main/java/com/lnks/siteautomacaoapi/controllers/CadastroUsuarioController.java
@@ -27,48 +27,33 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/api/cadastrar-usuario")
 @CrossOrigin(origins = "*")
 public class CadastroUsuarioController {
-    @Autowired
-    private ProfileService profileService;
+	@Autowired
+	private ProfileService profileService;
 
-    @PostMapping
-    public ResponseEntity<Response<CadastroUsuarioDto>> cadastrar(
-            @Valid @RequestBody CadastroUsuarioDto cadastroUsuarioDto, BindingResult result)
-            throws NoSuchAlgorithmException {
-        log.info("Cadastrando Usuario:{}", cadastroUsuarioDto.toString());
-        Response<CadastroUsuarioDto> response = new Response<CadastroUsuarioDto>();
-        validarUsuario(cadastroUsuarioDto, result);
-     
-        if(result.hasErrors()){
-            log.error("Erro na validação do cadastro do usuario: {}", result.getAllErrors());
-            result.getAllErrors().forEach(erro -> {
-            	if(response == null) {
-            		log.info("erro esta null");
-            	}else {
-            		log.info("erro nao esta null");
-            	}
-            	response.getErrors().add(erro.getDefaultMessage());
-            	log.info(erro.getDefaultMessage());
-            });
-            log.info("Erros adicionados {}", response.getErrors());
-            return ResponseEntity.badRequest().body(response);
-        }
-        
-        Profile novoProfile = Profile.builder()
-            .nome(cadastroUsuarioDto.getNome())
-            .email(cadastroUsuarioDto.getEmail())
-            .password(PasswordUtils.gerarBCrypt(cadastroUsuarioDto.getSenha()))
-            .build();
+	@PostMapping
+	public ResponseEntity<Response<CadastroUsuarioDto>> cadastrar(
+			@Valid @RequestBody CadastroUsuarioDto cadastroUsuarioDto, BindingResult result)
+			throws NoSuchAlgorithmException {
+		log.info("Cadastrando Usuario:{}", cadastroUsuarioDto.toString());
+		Response<CadastroUsuarioDto> response = new Response<CadastroUsuarioDto>();
+		validarUsuario(cadastroUsuarioDto, result);
 
-        log.info("salvando...");
-        this.profileService.salvarProfile(novoProfile);
-        log.info("profile foi salvo...");
-        response.setData(cadastroUsuarioDto);
-        return ResponseEntity.ok(response);
-    }
+		if (result.hasErrors()) {
+			result.getAllErrors().forEach(erro -> response.getErrors().add(erro.getDefaultMessage()));
+			return ResponseEntity.badRequest().body(response);
+		}
 
-    private void validarUsuario(CadastroUsuarioDto cadastroUsuarioDto, BindingResult result) {
-        log.info("Verificando email {}", cadastroUsuarioDto.getEmail());
-        this.profileService.buscarPorEmail(cadastroUsuarioDto.getEmail())
-                .ifPresent(perfil -> result.addError(new ObjectError("email", "email já cadastrado")));    
-    }
+		Profile novoProfile = Profile.builder().nome(cadastroUsuarioDto.getNome()).email(cadastroUsuarioDto.getEmail())
+				.password(PasswordUtils.gerarBCrypt(cadastroUsuarioDto.getSenha())).build();
+
+		this.profileService.salvarProfile(novoProfile);
+		response.setData(cadastroUsuarioDto);
+		return ResponseEntity.ok(response);
+	}
+
+	private void validarUsuario(CadastroUsuarioDto cadastroUsuarioDto, BindingResult result) {
+		log.info("Verificando email {}", cadastroUsuarioDto.getEmail());
+		this.profileService.buscarPorEmail(cadastroUsuarioDto.getEmail())
+				.ifPresent(perfil -> result.addError(new ObjectError("email", "email já cadastrado")));
+	}
 }
--- src/main/java/com/lnks/siteautomacaoapi/response/Response.java
+++ src/main/java/com/lnks/siteautomacaoapi/response/Response.java
@@ -1,11 +1,14 @@
 package com.lnks.siteautomacaoapi.response;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import lombok.Data;
+import lombok.Singular;
 
 @Data
 public class Response<T> {
     private T data;
-    private List<String> errors;       
+    private List<String> errors = new ArrayList<String>();    
+    
 }
